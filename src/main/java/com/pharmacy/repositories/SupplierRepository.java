43d243c21e332e5d63ccba73fac0dc7930324632
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-27 21:09:11 +1000

    feat: create function to find supplier by name and contact number

--- src/main/java/com/pharmacy/repositories/SupplierRepository.java
+++ src/main/java/com/pharmacy/repositories/SupplierRepository.java
@@ -8,4 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+    // Find a supplier by name
+    public Supplier findByName(String name);
+
+    // Find a supplier by contact
+    public Supplier findByContact(String contact);
 }
