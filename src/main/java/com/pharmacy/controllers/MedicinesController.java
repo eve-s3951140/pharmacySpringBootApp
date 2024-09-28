0651a884a85178bce0a52ad619d362ead6808252
eve-s3951140 <s3951140@student.rmit.edu.au>
2024-09-28 14:29:27 +1000

    refactor: order imports based on length

--- src/main/java/com/pharmacy/controllers/MedicinesController.java
+++ src/main/java/com/pharmacy/controllers/MedicinesController.java
@@ -7,8 +7,8 @@ import com.pharmacy.services.SupplierService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
